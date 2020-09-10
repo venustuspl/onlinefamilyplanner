b15fda44ed6fad871ff53bb42e37f6e83c931276
Tomek <venustus.pl@gmail.com>
2020-09-10 09:34:19 +0200

    First

---
+++ src/main/java/pl/Venustus/OnlineFamilyPlanner/OnlineFamilyPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.Venustus.OnlineFamilyPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFamilyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFamilyPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/Venustus/OnlineFamilyPlanner/OnlineFamilyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Venustus.OnlineFamilyPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFamilyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
