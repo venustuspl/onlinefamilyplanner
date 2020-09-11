3b1f3a5bbcaa9ae30f531f6584f67432e351c811
Tomek <venustus.pl@gmail.com>
2020-09-11 08:09:52 +0200

    0.0.3 Added SecurityConfig.java

--- src/main/java/pl/Venustus/OnlineFamilyPlanner/Controller/PlannerController.java
+++ src/main/java/pl/Venustus/OnlineFamilyPlanner/Controller/PlannerController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.*;
 public class PlannerController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
+    @ResponseBody
+    public String getStartPage() {
+        return "start";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/loggeduser")
     public String getIndexPage() {
         return "index";
     }
