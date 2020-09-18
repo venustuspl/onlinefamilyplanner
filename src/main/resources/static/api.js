8ded14697d25f764943397230c667a4f0800c073
venustuspl <venustus.pl@gmail.com>
2020-09-19 00:17:38 +0200

    0.1.7 Updated index.html

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -56,6 +56,7 @@ geteachday(day,step);
 function start(){
     getalldayofmonth2();
     console.log("start");
+    getcalendardays();
     //getalldayofmonth();
     //getalldayofmonthbyeach();
 
@@ -84,13 +85,31 @@ fetch('getalldayofmonth')
             document.getElementById( "day" + i + "status").value = rate.status;
             document.getElementById( "day" + i + "description").value = rate.description;
             document.getElementById( "day" + i + "note").value = rate.note;
-            console.log(rate.status);
-            console.log(rate.description);
-            console.log(rate.note);
-            console.log("day" + i + "status");
+           // console.log(rate.status);
+            //console.log(rate.description);
+            //console.log(rate.note);
+            //console.log("day" + i + "status");
             i++;
                   })
     });
+
+}
+
+function getcalendardays(){
+
+    const now = new Date();
+    //console.log(now);
+    console.log(now.getMonth());
+    console.log(now.getYear());
+    someDateAsString = new Date(now.getFullYear(), now.getMonth(), 1);
+    console.log(someDateAsString);
+    var calendarday = someDateAsString.getDay();
+
+    const days = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota","Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota","Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota","Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota","Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
+    console.log(days[calendarday]);
+    for (step = 1; step < 4; step++) {
+        document.getElementById("day" + step + "name").value = days[calendarday + step - 1];
+    }
 
 }
 
