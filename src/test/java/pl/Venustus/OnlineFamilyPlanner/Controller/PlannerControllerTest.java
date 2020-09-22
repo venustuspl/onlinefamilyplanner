faddbff3b63b99b5ee4dff2100cb912ef0097daa
Tomek <venustus.pl@gmail.com>
2020-09-22 15:15:19 +0200

    0.1.12 Refactor testSaveDayOfMonthDto_succes()

--- src/test/java/pl/Venustus/OnlineFamilyPlanner/Controller/PlannerControllerTest.java
+++ src/test/java/pl/Venustus/OnlineFamilyPlanner/Controller/PlannerControllerTest.java
@@ -2,7 +2,6 @@ package pl.Venustus.OnlineFamilyPlanner.Controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import pl.Venustus.OnlineFamilyPlanner.Domain.DayOfMonth;
@@ -19,17 +18,22 @@ class PlannerControllerTest {
         RestTemplate restTemplate = new RestTemplate();
         final String baseUrl = "http://localhost:8080/save";
         URI uri = new URI(baseUrl);
-        DayOfMonth day = new DayOfMonth((long) 1, "Adam", "Gilly", "test@email.com");
+        DayOfMonth day = new DayOfMonth((long) 1, "Maks", "aa", "aaa");
+        DayOfMonth day1 = new DayOfMonth((long) 2, "Tom", "aa", "aaa");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-COM-PERSIST", "true");
-        headers.set("X-COM-LOCATION", "USA");
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("X-COM-PERSIST", "true");
+//        headers.set("X-COM-LOCATION", "USA");
 
-        HttpEntity<DayOfMonth> request = new HttpEntity<>(day, headers);
+        HttpEntity<DayOfMonth> request = new HttpEntity<>(day);
+        HttpEntity<DayOfMonth> request1 = new HttpEntity<>(day1);
 
         ResponseEntity<String> result = restTemplate.postForEntity(uri, request, String.class);
+        System.out.println(result);
 
+        ResponseEntity<String> result1 = restTemplate.postForEntity(uri, request1, String.class);
+        System.out.println(result1);
         //Verify request succeed
-        assertEquals(201, result.getStatusCodeValue());
+        assertEquals(200, result.getStatusCodeValue());
     }
 }
