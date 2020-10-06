8d969e02517a5e0129c17b0df65e5f3a89160010
Tomek <venustus.pl@gmail.com>
2020-10-06 12:05:57 +0200

    2.0.3 Spring Security is back

--- src/main/java/pl/Venustus/OnlineFamilyPlanner/Configuration/SecurityConfig.java
+++ src/main/java/pl/Venustus/OnlineFamilyPlanner/Configuration/SecurityConfig.java
@@ -1,29 +1,33 @@
+
 package pl.Venustus.OnlineFamilyPlanner.Configuration;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("admin")
-//                .password("{noop}1234")
-//                .roles("USER", "ADMIN");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.antMatcher("/**")
-//                .authorizeRequests()
-//                .antMatchers("/images**", "/**", "/login**","/h2")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .formLogin();
-//    }
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//}
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}1234")
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/images**", "/**", "/login**", "/h2")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+    }
+
+}
