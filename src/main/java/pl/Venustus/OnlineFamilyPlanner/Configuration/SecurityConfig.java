95cb4ce1d6bfe3a8ea364e1e22879157cc2db856
venustuspl <venustus.pl@gmail.com>
2020-10-13 23:14:16 +0200

    2.0.4 SecurityConfig.java upgrading

--- src/main/java/pl/Venustus/OnlineFamilyPlanner/Configuration/SecurityConfig.java
+++ src/main/java/pl/Venustus/OnlineFamilyPlanner/Configuration/SecurityConfig.java
@@ -22,8 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/images**", "/", "/login**", "/h2", "/sendMail")
+                .antMatchers("/images**", "/", "/login**")
                 .permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/h2/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
