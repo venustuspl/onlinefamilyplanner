27b841222b01b285eedd9e40d2cae7cb7aa5538b
Tomek <venustus.pl@gmail.com>
2020-10-20 13:22:35 +0200

    3.0.4 Heroku deploy

---
+++ src/main/java/pl/Venustus/OnlineFamilyPlanner/Configuration/SecurityConfig.java
@@ -0,0 +1,39 @@
+
+package pl.Venustus.OnlineFamilyPlanner.Configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}1986")
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/images**", "/", "/login**")
+                .permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/h2/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+}
