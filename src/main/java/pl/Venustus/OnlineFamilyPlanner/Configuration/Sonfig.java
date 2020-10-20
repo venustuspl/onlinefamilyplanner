a1c6ef52944445f61d40cf83d4325bef186cb724
Tomek <venustus.pl@gmail.com>
2020-10-20 13:10:14 +0200

    3.0.3 Heroku deploy

--- src/main/java/pl/Venustus/OnlineFamilyPlanner/Configuration/Sonfig.java
+++ src/main/java/pl/Venustus/OnlineFamilyPlanner/Configuration/Sonfig.java
@@ -1,13 +1,12 @@
 
 package pl.Venustus.OnlineFamilyPlanner.Configuration;
 
-import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-public class SecurityConfig
+//@Configuration
+public class Sonfig
 // extends WebSecurityConfigurerAdapter
 {
 //
