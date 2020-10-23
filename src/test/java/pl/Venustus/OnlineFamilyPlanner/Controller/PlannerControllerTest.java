d8273bda3a96f2b0c3dd5d797c1559e9fc469b0e
venustuspl <venustus.pl@gmail.com>
2020-10-23 20:01:42 +0200

    2.0.19 The old test has been commented out

--- src/test/java/pl/Venustus/OnlineFamilyPlanner/Controller/PlannerControllerTest.java
+++ src/test/java/pl/Venustus/OnlineFamilyPlanner/Controller/PlannerControllerTest.java
@@ -1,37 +1,26 @@
 package pl.Venustus.OnlineFamilyPlanner.Controller;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-import pl.Venustus.OnlineFamilyPlanner.Domain.DayOfMonth;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class PlannerControllerTest {
 
-    @Test
-    void testSaveDayOfMonthDto_succes() throws URISyntaxException {
-        RestTemplate restTemplate = new RestTemplate();
-        final String baseUrl = "http://localhost:8080/save";
-        URI uri = new URI(baseUrl);
-        DayOfMonth day = new DayOfMonth((long) 4, "Maks", "aa", "aaa", "2020-01-01 01:01:01");
-        DayOfMonth day1 = new DayOfMonth((long) 5, "Tom", "aa", "aaa", "2020-01-01 01:01:01");
-
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("X-COM-PERSIST", "true");
-//        headers.set("X-COM-LOCATION", "USA");
-
-        HttpEntity<DayOfMonth> request = new HttpEntity<>(day);
-        HttpEntity<DayOfMonth> request1 = new HttpEntity<>(day1);
-
-        ResponseEntity<String> result = restTemplate.postForEntity(uri, request, String.class);
-
-        ResponseEntity<String> result1 = restTemplate.postForEntity(uri, request1, String.class);
-
-        assertEquals(200, result.getStatusCodeValue());
-    }
+//    @Test
+//    void testSaveDayOfMonthDto_succes() throws URISyntaxException {
+//        RestTemplate restTemplate = new RestTemplate();
+//        final String baseUrl = "http://localhost:8080/save";
+//        URI uri = new URI(baseUrl);
+//        DayOfMonth day = new DayOfMonth((long) 4, "Maks", "aa", "aaa", "2020-01-01 01:01:01");
+//        DayOfMonth day1 = new DayOfMonth((long) 5, "Tom", "aa", "aaa", "2020-01-01 01:01:01");
+//
+////        HttpHeaders headers = new HttpHeaders();
+////        headers.set("X-COM-PERSIST", "true");
+////        headers.set("X-COM-LOCATION", "USA");
+//
+//        HttpEntity<DayOfMonth> request = new HttpEntity<>(day);
+//        HttpEntity<DayOfMonth> request1 = new HttpEntity<>(day1);
+//
+//        ResponseEntity<String> result = restTemplate.postForEntity(uri, request, String.class);
+//
+//        ResponseEntity<String> result1 = restTemplate.postForEntity(uri, request1, String.class);
+//
+//        assertEquals(200, result.getStatusCodeValue());
+//    }
 }
